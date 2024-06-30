f2e1b7bfa85dbbacef91184d4ea1f313d76d8ece
nashiusagi <ritotech0226@gmail.com>
2024-06-30 11:55:38 +0900

    [update] GetAllのテスト追加

--- repositories/post_repository_test.go
+++ repositories/post_repository_test.go
@@ -13,10 +13,51 @@ import (
 
 func NewDbMock() (*gorm.DB, sqlmock.Sqlmock, error) {
 	_, mock, err := sqlmock.New()
+	if err != nil {
+		return nil, mock, err
+	}
+
 	mockDB, err := gorm.Open(sqlite.Open("../resources/post.db"), &gorm.Config{})
 
 	return mockDB, mock, err
 }
+
+func TestGetAll(t *testing.T) {
+	// Arrange
+	mockDB, mock, err := NewDbMock()
+
+	if err != nil {
+		t.Errorf("Failed to initialize mock DB: %v", err)
+	}
+
+	rows := sqlmock.
+		NewRows([]string{"id", "title", "body"}).
+		AddRow(uint(1), "title1", "body1")
+
+	mock.
+		ExpectQuery(
+			"SELECT * FROM posts",
+		).
+		WithArgs(1).
+		WillReturnRows(rows)
+
+	// Act
+	postRepository := repositories.NewPostRepository(mockDB)
+	posts, err := postRepository.GetAll()
+
+	// Assert
+	assert.Equal(t, err, nil)
+	assert.Equal(t, posts[0].ID, uint(1))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// if err := mock.ExpectationsWereMet(); err != nil {
+	// 	t.Errorf("Test Find Posts: %v", err)
+	// }
+}
+
 
 func TestFindById(t *testing.T) {
 	// Arrange
