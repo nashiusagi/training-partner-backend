004aff51a432fc7a52f714f0dd28212a62495f8f
nashiusagi <ritotech0226@gmail.com>
2024-09-07 15:48:34 +0900

    [update] fmt

--- internal/domains/Exercise.go
+++ internal/domains/Exercise.go
@@ -1,7 +1,7 @@
 package domains
 
 type Exercise struct {
-	ExerciseId       uint `gorm:"primary_key"`
+	ExerciseId   uint `gorm:"primary_key"`
 	Name         string
 	RegisteredId uint
 	Muscles      []Muscle `gorm:"many2many:exercise_muscles_target_to_train;joinForeignKey:ExerciseId;joinReferences:MuscleId"`
