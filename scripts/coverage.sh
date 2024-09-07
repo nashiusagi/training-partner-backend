9aea3d9622be1d0a277ae38acb3e35ba6c1ee4ad
nashiusagi <ritotech0226@gmail.com>
2024-09-07 13:24:52 +0900

    [fix] percentの値は任意

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
 percent=$(make coverage | grep lines | sed -r 's/[^0-9]*(.*\.[0-9]*)%.*/\1/' | sed -e 's/%/%25/')
-int=${percent%.*}
-if [ $int -gt 90 ]; then
-  curl -o outputs/coverage.svg https://img.shields.io/badge/coverage-${percent}%25-green
-elif [ $int -gt 75 ]; then
-  curl -o outputs/coverage.svg https://img.shields.io/badge/coverage-${percent}%25-yellow
-else
-  curl -o outputs/coverage.svg https://img.shields.io/badge/coverage-${percent}%25-red
-fi
+curl -o outputs/coverage.svg https://img.shields.io/badge/coverage-${percent}%25-green
