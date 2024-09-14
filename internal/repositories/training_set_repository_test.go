b091562e3a084e6a990b8ecb42a7b06d9e258ee2
nashiusagi <ritotech0226@gmail.com>
2024-09-14 14:22:06 +0900

    [update] trainingSetRepositoryのテストをリファクタ

--- internal/repositories/training_set_repository_test.go
+++ internal/repositories/training_set_repository_test.go
@@ -1,6 +1,7 @@
 package repositories_test
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 	"training-partner/internal/repositories"
@@ -10,81 +11,131 @@ import (
 )
 
 func TestTrainingSetRepositoryGetAll(t *testing.T) {
-	// Arrange
 	mockDB, mock, err := NewDbMock()
 	if err != nil {
 		t.Errorf("Failed to initialize mock DB: %v", err)
 	}
-	rows := sqlmock.
-		NewRows([]string{"training_set_id", "exercise_id", "weight", "repetition"}).
-		AddRow(uint(1), uint(1), uint(95), uint(10)).
-		AddRow(uint(1), uint(1), uint(85), uint(10))
-
-	mock.
-		ExpectQuery(
-			regexp.QuoteMeta("SELECT * FROM `training_sets`")).
-		WillReturnRows(rows)
-
-	// Act
 	trainingSetRepository := repositories.NewTrainingSetRepository(mockDB)
-	exercises, err := trainingSetRepository.GetAll()
 
-	// Assert
-	assert.Equal(t, err, nil)
-	assert.Equal(t, exercises[0].Weight, uint(95))
+	t.Run("正常に値を取得できた場合、TrainingSetを全て返す", func(t *testing.T) {
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `training_sets`")).
+			WillReturnRows(sqlmock.
+				NewRows([]string{"training_set_id", "exercise_id", "weight", "repetition"}).
+				AddRow(uint(1), uint(1), uint(95), uint(10)).
+				AddRow(uint(2), uint(1), uint(85), uint(10)))
 
-	if err != nil {
-		t.Fatal(err)
-	}
+		training_sets, err := trainingSetRepository.GetAll()
+
+		assert.Equal(t, nil, err)
+		assert.Equal(t, uint(95), training_sets[0].Weight)
+		assert.Equal(t, uint(85), training_sets[1].Weight)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Find Training Sets: %v", err)
+		}
+	})
+
+	t.Run("正常に値を取得できない場合、エラーを返す", func(t *testing.T) {
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `training_sets`")).
+			WillReturnError(fmt.Errorf("some errors"))
+
+		_, err := trainingSetRepository.GetAll()
+
+		if err == nil {
+			t.Errorf("error is expected, but error not occured")
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Find TrainingSets: %v", err)
+		}
+	})
 }
 
 func TestTrainingSetRepositoryFindById(t *testing.T) {
-	// Arrange
 	mockDB, mock, err := NewDbMock()
-
 	if err != nil {
 		t.Errorf("Failed to initialize mock DB: %v", err)
 	}
-
-	rows := sqlmock.
-		NewRows([]string{"training_set_id", "exercise_id", "weight", "repetition"}).
-		AddRow(uint(1), uint(1), uint(95), uint(10)).
-		AddRow(uint(1), uint(1), uint(85), uint(10))
-
-	mock.
-		ExpectQuery(
-			regexp.QuoteMeta("SELECT * FROM `training_sets` WHERE `training_sets`.`training_set_id` = ?")).
-		WithArgs(1).
-		WillReturnRows(rows)
-
-	// Act
 	trainingSetRepository := repositories.NewTrainingSetRepository(mockDB)
-	trainingSet, err := trainingSetRepository.FindById(1)
 
-	// Assert
-	assert.Equal(t, err, nil)
-	assert.Equal(t, trainingSet.TrainingSetId, uint(1))
-	assert.Equal(t, trainingSet.Weight, uint(95))
+	t.Run("正常に値を取得できた場合、対応するExerciseを返す", func(t *testing.T) {
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `training_sets` WHERE `training_sets`.`training_set_id` = ?")).
+			WithArgs(1).
+			WillReturnRows(sqlmock.
+				NewRows([]string{"training_set_id", "exercise_id", "weight", "repetition"}).
+				AddRow(uint(1), uint(1), uint(95), uint(10)))
 
-	if err != nil {
-		t.Fatal(err)
-	}
+		trainingSet, err := trainingSetRepository.FindById(1)
+
+		assert.Equal(t, nil, err)
+		assert.Equal(t, uint(1), trainingSet.TrainingSetId)
+		assert.Equal(t, uint(95), trainingSet.Weight)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Find TrainingSets: %v", err)
+		}
+	})
+
+	t.Run("正常に値を取得できない場合、エラーを返す", func(t *testing.T) {
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `training_sets` WHERE `training_sets`.`training_set_id` = ?")).
+			WithArgs(1).
+			WillReturnError(fmt.Errorf("some errors"))
+
+		_, err := trainingSetRepository.FindById(1)
+
+		if err == nil {
+			t.Errorf("error is expected, but error not occured")
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Find TrainingSets: %v", err)
+		}
+	})
 }
 
 func TestTrainingSetRepositoryCreate(t *testing.T) {
 	mockDB, mock, err := NewDbMock()
-
 	if err != nil {
 		t.Errorf("Failed to initialize mock DB: %v", err)
 	}
-
-	mock.ExpectBegin()
-	mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `training_sets` (`exercise_id`,`weight`,`repetition`) VALUES (?,?,?)")).WithArgs(uint(1), uint(105), uint(10)).WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectCommit()
-
 	trainingSetRepository := repositories.NewTrainingSetRepository(mockDB)
-	err = trainingSetRepository.Create(uint(1), uint(105), uint(10))
 
-	// Assert
-	assert.Equal(t, err, nil)
+	t.Run("正常にTrainingSetを作成できた場合、nilを返す", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `training_sets` (`exercise_id`,`weight`,`repetition`) VALUES (?,?,?)")).WithArgs(uint(1), uint(105), uint(10)).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectCommit()
+
+		err = trainingSetRepository.Create(uint(1), uint(105), uint(10))
+
+		assert.Equal(t, nil, err)
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Create TrainingSet: %v", err)
+		}
+	})
+
+	t.Run("正常にTrainingSetを作成できなかった場合、作成がされずエラーを返す", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `training_sets` (`exercise_id`,`weight`,`repetition`) VALUES (?,?,?)")).WithArgs(uint(1), uint(105), uint(10)).WillReturnError(fmt.Errorf("some errors"))
+		mock.ExpectRollback()
+
+		err = trainingSetRepository.Create(uint(1), uint(105), uint(10))
+
+		if err == nil {
+			t.Errorf("error is expected, but error not occured")
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Create Menu: %v", err)
+		}
+	})
+
 }
