0e138031cc5125a7ef4492f69053e4c8fd9db471
nashiusagi <ritotech0226@gmail.com>
2024-09-14 13:02:07 +0900

    [update] mockDBを使えるように改修

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -40,6 +41,7 @@ require (
 	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/mysql v1.5.7 // indirect
 	gorm.io/driver/sqlite v1.5.6 // indirect
 	gorm.io/gorm v1.25.10 // indirect
 )
--- internal/repositories/exercise_repository_test.go
+++ internal/repositories/exercise_repository_test.go
@@ -7,39 +7,51 @@ import (
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func NewDbMock() (*gorm.DB, sqlmock.Sqlmock, error) {
-	_, mock, err := sqlmock.New()
+	mockDB, mock, err := sqlmock.New()
 	if err != nil {
 		return nil, mock, err
 	}
 
-	mockDB, err := gorm.Open(sqlite.Open("../../resources/training_partner.db"), &gorm.Config{})
+	// NOTE: sqliteのモックはできなさそうなので、mysqlを使用している
+	db, err := gorm.Open(mysql.New(mysql.Config{Conn: mockDB, SkipInitializeWithVersion: true}), &gorm.Config{})
 
-	return mockDB, mock, err
+	return db, mock, err
 }
 
 func TestExerciseRepositoryGetAll(t *testing.T) {
 	// Arrange
 	mockDB, mock, err := NewDbMock()
-
 	if err != nil {
 		t.Errorf("Failed to initialize mock DB: %v", err)
 	}
-
-	rows := sqlmock.
-		NewRows([]string{"id", "title", "body"}).
-		AddRow(uint(1), "title1", "body1")
-
+	exerciseRows := sqlmock.
+		NewRows([]string{"exercise_id", "name", "registered_id"}).
+		AddRow(uint(1), "スクワット", uint(5))
+	exerciseMusclesRows := sqlmock.
+		NewRows([]string{"id", "exercise_id", "muscle_id"}).
+		AddRow(uint(1), uint(1), uint(1))
+	muscleRows := sqlmock.
+		NewRows([]string{"muscle_id", "name", "body_part_id"}).
+		AddRow(uint(1), "大腿四頭筋", uint(7))
 	mock.
 		ExpectQuery(
-			"SELECT * FROM exercises",
-		).
+			regexp.QuoteMeta("SELECT * FROM `exercises`")).
+		WillReturnRows(exerciseRows)
+	mock.
+		ExpectQuery(
+			regexp.QuoteMeta("SELECT * FROM `exercise_muscles_target_to_train` WHERE `exercise_muscles_target_to_train`.`exercise_id` = ?")).
 		WithArgs(1).
-		WillReturnRows(rows)
+		WillReturnRows(exerciseMusclesRows)
+	mock.
+		ExpectQuery(
+			regexp.QuoteMeta("SELECT * FROM `muscles` WHERE `muscles`.`muscle_id` = ?")).
+		WithArgs(1).
+		WillReturnRows(muscleRows)
 
 	// Act
 	exerciseRepository := repositories.NewExerciseRepository(mockDB)
@@ -66,16 +78,31 @@ func TestExerciseRepositoryFindById(t *testing.T) {
 		t.Errorf("Failed to initialize mock DB: %v", err)
 	}
 
-	rows := sqlmock.
-		NewRows([]string{"id", "title", "body"}).
-		AddRow(uint(1), "title1", "body1")
+	exerciseRows := sqlmock.
+		NewRows([]string{"exercise_id", "name", "registered_id"}).
+		AddRow(uint(1), "スクワット", uint(5))
+	exerciseMusclesRows := sqlmock.
+		NewRows([]string{"id", "exercise_id", "muscle_id"}).
+		AddRow(uint(1), uint(1), uint(1))
+	muscleRows := sqlmock.
+		NewRows([]string{"muscle_id", "name", "body_part_id"}).
+		AddRow(uint(1), "大腿四頭筋", uint(7))
 
 	mock.
-		ExpectQuery(regexp.QuoteMeta(
-			`SELECT * FROM "exercises" WHERE id = ?`,
-		)).
+		ExpectQuery(
+			regexp.QuoteMeta("SELECT * FROM `exercises` WHERE `exercises`.`exercise_id` = ?")).
 		WithArgs(1).
-		WillReturnRows(rows)
+		WillReturnRows(exerciseRows)
+	mock.
+		ExpectQuery(
+			regexp.QuoteMeta("SELECT * FROM `exercise_muscles_target_to_train` WHERE `exercise_muscles_target_to_train`.`exercise_id` = ?")).
+		WithArgs(1).
+		WillReturnRows(exerciseMusclesRows)
+	mock.
+		ExpectQuery(
+			regexp.QuoteMeta("SELECT * FROM `muscles` WHERE `muscles`.`muscle_id` = ?")).
+		WithArgs(1).
+		WillReturnRows(muscleRows)
 
 	// Act
 	exerciseRepository := repositories.NewExerciseRepository(mockDB)
