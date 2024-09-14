b4bfbd680e5fa6e43dfbfeeade7e2a901d161d4a
nashiusagi <ritotech0226@gmail.com>
2024-09-14 14:07:18 +0900

    [update] muscleRepositoryのテストをリファクタ

--- internal/repositories/muscle_repository_test.go
+++ internal/repositories/muscle_repository_test.go
@@ -1,6 +1,7 @@
 package repositories_test
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 	"training-partner/internal/repositories"
@@ -10,65 +11,92 @@ import (
 )
 
 func TestMuscleRepositoryGetAll(t *testing.T) {
-	// Arrange
 	mockDB, mock, err := NewDbMock()
 	if err != nil {
 		t.Errorf("Failed to initialize mock DB: %v", err)
 	}
-	muscleRows := sqlmock.
-		NewRows([]string{"muscle_id", "name", "body_part_id"}).
-		AddRow(uint(1), "大腿四頭筋", uint(7))
-	mock.
-		ExpectQuery(
-			regexp.QuoteMeta("SELECT * FROM `muscles`")).
-		WillReturnRows(muscleRows)
-
-	// Act
 	muscleRepository := repositories.NewMuscleRepository(mockDB)
-	muscles, err := muscleRepository.GetAll()
 
-	// Assert
-	assert.Equal(t, err, nil)
-	assert.Equal(t, muscles[0].MuscleId, uint(1))
+	t.Run("正常に値を取得できた場合、Muscleを全て返す", func(t *testing.T) {
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `muscles`")).
+			WillReturnRows(sqlmock.
+				NewRows([]string{"muscle_id", "name", "body_part_id"}).
+				AddRow(uint(1), "大腿四頭筋", uint(7)))
 
-	if err != nil {
-		t.Fatal(err)
-	}
+		muscles, err := muscleRepository.GetAll()
 
-	// if err := mock.ExpectationsWereMet(); err != nil {
-	// 	t.Errorf("Test Find Menus: %v", err)
-	// }
+		assert.Equal(t, nil, err)
+		assert.Equal(t, "大腿四頭筋", muscles[0].Name)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Find Muscles: %v", err)
+		}
+	})
+
+	t.Run("正常に値を取得できない場合、エラーを返す", func(t *testing.T) {
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `muscles`")).
+			WillReturnError(fmt.Errorf("some errors"))
+
+		_, err := muscleRepository.GetAll()
+
+		if err == nil {
+			t.Errorf("error is expected, but error not occured")
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Find Muscles: %v", err)
+		}
+	})
 }
 
 func TestMuscleRepositoryFindById(t *testing.T) {
-	// Arrange
 	mockDB, mock, err := NewDbMock()
 	if err != nil {
 		t.Errorf("Failed to initialize mock DB: %v", err)
 	}
-	muscleRows := sqlmock.
-		NewRows([]string{"muscle_id", "name", "body_part_id"}).
-		AddRow(uint(1), "大腿四頭筋", uint(7))
-	mock.
-		ExpectQuery(
-			regexp.QuoteMeta("SELECT * FROM `muscles` WHERE `muscles`.`muscle_id` = ?")).
-		WithArgs(1).
-		WillReturnRows(muscleRows)
-
-	// Act
 	muscleRepository := repositories.NewMuscleRepository(mockDB)
-	muscle, err := muscleRepository.FindById(1)
 
-	// Assert
-	assert.Equal(t, err, nil)
-	assert.Equal(t, muscle.MuscleId, uint(1))
-	assert.Equal(t, muscle.Name, "大腿四頭筋")
+	t.Run("正常に値を取得できた場合、Muscleを全て返す", func(t *testing.T) {
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `muscles` WHERE `muscles`.`muscle_id` = ?")).
+			WithArgs(1).
+			WillReturnRows(sqlmock.
+				NewRows([]string{"muscle_id", "name", "body_part_id"}).
+				AddRow(uint(1), "大腿四頭筋", uint(7)))
 
-	if err != nil {
-		t.Fatal(err)
-	}
+		muscle, err := muscleRepository.FindById(1)
 
-	// if err := mock.ExpectationsWereMet(); err != nil {
-	// 	t.Errorf("Test Find Menus: %v", err)
-	// }
+		assert.Equal(t, nil, err)
+		assert.Equal(t, uint(1), muscle.MuscleId)
+		assert.Equal(t, "大腿四頭筋", muscle.Name)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Find Muscles: %v", err)
+		}
+	})
+
+	t.Run("正常に値を取得できない場合、エラーを返す", func(t *testing.T) {
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `muscles` WHERE `muscles`.`muscle_id` = ?")).
+			WithArgs(1).
+			WillReturnError(fmt.Errorf("some errors"))
+
+		_, err := muscleRepository.FindById(1)
+
+		if err == nil {
+			t.Errorf("error is expected, but error not occured")
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Find Muscles: %v", err)
+		}
+	})
 }
