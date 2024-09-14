f96a6b22a23b56f194a8d4cef0949122443689fc
nashiusagi <ritotech0226@gmail.com>
2024-09-14 13:56:12 +0900

    [update] menuRepositoryのテストをリファクタ

--- internal/repositories/menu_repository_test.go
+++ internal/repositories/menu_repository_test.go
@@ -1,6 +1,7 @@
 package repositories_test
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 	"time"
@@ -15,36 +16,54 @@ func TestMenuRepositoryGetAll(t *testing.T) {
 	if err != nil {
 		t.Errorf("Failed to initialize mock DB: %v", err)
 	}
-	menuRows := sqlmock.
-		NewRows([]string{"menu_id", "date"}).
-		AddRow(uint(1), time.Date(2020, 4, 1, 0, 0, 0, 0, time.UTC))
-	menuTrainingTargetRows := sqlmock.
-		NewRows([]string{"menu_id", "training_set_id", "count"}).
-		AddRow(uint(1), uint(1), uint(3))
-	trainingSetRows := sqlmock.
-		NewRows([]string{"training_set_id", "exercise_id", "weight", "repetition"}).
-		AddRow(uint(1), uint(1), uint(95), uint(10))
-	mock.
-		ExpectQuery(
-			regexp.QuoteMeta("SELECT * FROM `menus`")).
-		WillReturnRows(menuRows)
-	mock.
-		ExpectQuery(
-			regexp.QuoteMeta("SELECT * FROM `menus_training_sets` WHERE `menus_training_sets`.`menu_id` = ?")).
-		WithArgs(1).
-		WillReturnRows(menuTrainingTargetRows)
-	mock.
-		ExpectQuery(
-			regexp.QuoteMeta("SELECT * FROM `training_sets` WHERE `training_sets`.`training_set_id` = ?")).
-		WithArgs(1).
-		WillReturnRows(trainingSetRows)
-
 	menuRepository := repositories.NewMenuRepository(mockDB)
-	menus, err := menuRepository.GetAll()
 
-	// TODO: mockを直す
-	assert.Equal(t, nil, err)
-	assert.Equal(t, time.Date(2020, 4, 1, 0, 0, 0, 0, time.UTC), menus[0].Date)
+	t.Run("正常に値を取得できた場合、Exerciseを全て返す", func(t *testing.T) {
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `menus`")).
+			WillReturnRows(sqlmock.
+				NewRows([]string{"menu_id", "date"}).
+				AddRow(uint(1), time.Date(2020, 4, 1, 0, 0, 0, 0, time.UTC)))
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `menus_training_sets` WHERE `menus_training_sets`.`menu_id` = ?")).
+			WithArgs(1).
+			WillReturnRows(sqlmock.
+				NewRows([]string{"menu_id", "training_set_id", "count"}).
+				AddRow(uint(1), uint(1), uint(3)))
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `training_sets` WHERE `training_sets`.`training_set_id` = ?")).
+			WithArgs(1).
+			WillReturnRows(sqlmock.
+				NewRows([]string{"training_set_id", "exercise_id", "weight", "repetition"}).
+				AddRow(uint(1), uint(1), uint(95), uint(10)))
+
+		menus, err := menuRepository.GetAll()
+
+		assert.Equal(t, nil, err)
+		assert.Equal(t, time.Date(2020, 4, 1, 0, 0, 0, 0, time.UTC), menus[0].Date)
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Find Exercises: %v", err)
+		}
+	})
+
+	t.Run("正常に値を取得できない場合、エラーを返す", func(t *testing.T) {
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `menus`")).
+			WillReturnError(fmt.Errorf("some errors"))
+
+		_, err := menuRepository.GetAll()
+
+		if err == nil {
+			t.Errorf("error is expected, but error not occured")
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Find Menus: %v", err)
+		}
+	})
 }
 
 func TestMenuRepositoryFindById(t *testing.T) {
@@ -52,52 +71,88 @@ func TestMenuRepositoryFindById(t *testing.T) {
 	if err != nil {
 		t.Errorf("Failed to initialize mock DB: %v", err)
 	}
-	menuRows := sqlmock.
-		NewRows([]string{"menu_id", "date"}).
-		AddRow(uint(1), time.Date(2020, 4, 1, 0, 0, 0, 0, time.UTC))
-	menuTrainingTargetRows := sqlmock.
-		NewRows([]string{"menu_id", "training_set_id", "count"}).
-		AddRow(uint(1), uint(1), uint(3))
-	trainingSetRows := sqlmock.
-		NewRows([]string{"training_set_id", "exercise_id", "weight", "repetition"}).
-		AddRow(uint(1), uint(1), uint(95), uint(10))
-	mock.
-		ExpectQuery(
-			regexp.QuoteMeta("SELECT * FROM `menus` WHERE `menus`.`menu_id` = ?")).
-		WithArgs(1).
-		WillReturnRows(menuRows)
-	mock.
-		ExpectQuery(
-			regexp.QuoteMeta("SELECT * FROM `menus_training_sets` WHERE `menus_training_sets`.`menu_id` = ?")).
-		WithArgs(1).
-		WillReturnRows(menuTrainingTargetRows)
-	mock.
-		ExpectQuery(
-			regexp.QuoteMeta("SELECT * FROM `training_sets` WHERE `training_sets`.`training_set_id` = ?")).
-		WithArgs(1).
-		WillReturnRows(trainingSetRows)
-
 	menuRepository := repositories.NewMenuRepository(mockDB)
-	menu, err := menuRepository.FindById(1)
 
-	assert.Equal(t, nil, err)
-	assert.Equal(t, time.Date(2020, 4, 1, 0, 0, 0, 0, time.UTC), menu.Date)
+	t.Run("正常に値を取得できた場合、Exerciseを全て返す", func(t *testing.T) {
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `menus` WHERE `menus`.`menu_id` = ?")).
+			WithArgs(1).
+			WillReturnRows(sqlmock.
+				NewRows([]string{"menu_id", "date"}).
+				AddRow(uint(1), time.Date(2020, 4, 1, 0, 0, 0, 0, time.UTC)))
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `menus_training_sets` WHERE `menus_training_sets`.`menu_id` = ?")).
+			WithArgs(1).
+			WillReturnRows(sqlmock.
+				NewRows([]string{"menu_id", "training_set_id", "count"}).
+				AddRow(uint(1), uint(1), uint(3)))
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `training_sets` WHERE `training_sets`.`training_set_id` = ?")).
+			WithArgs(1).
+			WillReturnRows(sqlmock.
+				NewRows([]string{"training_set_id", "exercise_id", "weight", "repetition"}).
+				AddRow(uint(1), uint(1), uint(95), uint(10)))
+
+		menu, err := menuRepository.FindById(1)
+
+		assert.Equal(t, nil, err)
+		assert.Equal(t, time.Date(2020, 4, 1, 0, 0, 0, 0, time.UTC), menu.Date)
+	})
+
+	t.Run("正常に値を取得できない場合、エラーを返す", func(t *testing.T) {
+		mock.
+			ExpectQuery(
+				regexp.QuoteMeta("SELECT * FROM `menus` WHERE `menus`.`menu_id` = ?")).
+			WithArgs(1).
+			WillReturnError(fmt.Errorf("some errors"))
+
+		_, err := menuRepository.FindById(1)
+
+		if err == nil {
+			t.Errorf("error is expected, but error not occured")
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Find Menus: %v", err)
+		}
+	})
 }
 
 func TestMenuRepositoryCreate(t *testing.T) {
 	mockDB, mock, err := NewDbMock()
-
 	if err != nil {
 		t.Errorf("Failed to initialize mock DB: %v", err)
 	}
-
-	mock.ExpectBegin()
-	mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `menus` (`date`) VALUES (?)")).WithArgs(time.Date(2024, 12, 1, 0, 0, 0, 0, time.UTC)).WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectCommit()
-
 	menuRepository := repositories.NewMenuRepository(mockDB)
-	err = menuRepository.Create(time.Date(2024, 12, 1, 0, 0, 0, 0, time.UTC))
 
-	// Assert
-	assert.Equal(t, nil, err)
+	t.Run("正常にMenuを作成できた場合、nilを返す", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `menus` (`date`) VALUES (?)")).WithArgs(time.Date(2024, 12, 1, 0, 0, 0, 0, time.UTC)).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectCommit()
+
+		err = menuRepository.Create(time.Date(2024, 12, 1, 0, 0, 0, 0, time.UTC))
+
+		assert.Equal(t, nil, err)
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Create Menu: %v", err)
+		}
+	})
+
+	t.Run("正常にMenuを作成できなかった場合、作成がされずエラーを返す", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `menus` (`date`) VALUES (?)")).WithArgs(time.Date(2024, 12, 1, 0, 0, 0, 0, time.UTC)).WillReturnError(fmt.Errorf("some errors"))
+		mock.ExpectRollback()
+
+		err = menuRepository.Create(time.Date(2024, 12, 1, 0, 0, 0, 0, time.UTC))
+
+		if err == nil {
+			t.Errorf("error is expected, but error not occured")
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Test Create Menu: %v", err)
+		}
+	})
+
 }
